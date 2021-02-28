73ac6d07c58dce5b0982d425296c5c14e0d5062e
home_n_belov <n.belov@cbonds.info>
2021-02-28 19:52:11 +0300

    Тестовый парсер v1

--- PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1.py
@@ -1,50 +1,82 @@
-import random
 import requests
+import re
+import locale
+from bs4 import BeautifulSoup as soap
+
+results = {}
 
 
-def good_password_gen(length=10):
-    letters = 'abcdefghijklmnopqrstuvwxyz'
-    upper_letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-    numbers = '0123456789'
-    symbols = '!@#$%^&*()_+-=\'\\\"'
-    alphabet = letters + upper_letters + numbers + symbols
+def run():
+    url = 'https://www.borsaitaliana.it/borsa/azioni/' \
+          'obbligazioni-convertibili/dati-completi.html?isin=IT0005256059&lang=en'
 
-    password = ''
-    for i in range(length):
-        password += random.choice(alphabet)
+    locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
 
-    # print('Сгенерирован пароль' + length + ' символов')
-    return password
+    isin = re.search(r'isin=(\w+)', url, re.I).group(1)
+    print(isin)
+    # resource = requests.get(url)
+    # if not resource.text:
+    #     print('Page is not found: ' + url)
+    #     return ''
+
+    # f = open('D:/Documents/test.html', 'w')
+    # f.write(resource.text)
+    # f.close()
+    file = open('D:/Documents/test.html', 'r')
+    content = file.read()
+    file.close()
+
+    doc = soap(content, 'html.parser')
+    # rows = doc.find_all('table')[0].find_all('tr')
+    rows = doc.find_all(string=re.compile('Stream Prices'))[0].find_parent('table').find_all('tr')
+    if not rows:
+        print('Table is empty')
+        return
+
+    columns = {}
+    columns_counter = 0
+    patterns = {
+        r'Stream\s+Prices'    : 'stream',
+        r'No'                 : 'number',
+        r'Bid\s+Quantity'     : 'bid_quantity',
+        r'Ask\s+Quantity'     : 'ask_quantity',
+        r'Bid\s+Price'        : 'bid',
+        r'Ask\s+Price'        : 'ask',
+    }
+
+    keys = results.keys()
+    for row in rows:
+        if columns_counter < 5:
+            columns = get_columns(row, patterns.items())
+            columns_counter = columns.__len__()
+            continue
+
+        for column, index in columns.items():
+            print(row.find_all('td')[index].text)
+            value = row.find_all('td')[index].text.strip()
+            if not value:
+                continue
+
+            value = locale.atof(value)
+            if value:
+                if isin in keys:
+                    results[isin][column] = value
+                else:
+                    results[isin] = {column : value}
+
+    print(results)
 
 
-counter = 0
+def get_columns(row, patterns, tag='th'):
+    columns = {}
+    index = 0
+    for cell in row.find_all(tag):
+        for pattern, column in patterns:
+            if re.findall(pattern, cell.text, re.I):
+                columns[column] = index
+        index += 1
+
+    return columns
 
 
-def bad_password_gen():
-    global counter
-    passwords = """password
-123456
-12345678
-1234
-qwerty
-12345
-dragon
-pussy
-baseball
-football"""
-
-    popular_passwords = passwords.split('\n')
-    counter += 1
-    while counter >= len(popular_passwords):
-        counter = counter - len(popular_passwords)
-
-    return popular_passwords[counter]
-
-
-while True:
-    password = bad_password_gen()
-    response = requests.post('http://127.0.0.1:')
-
-print(good_password_gen(6))
-print(good_password_gen(7))
-print(good_password_gen(8))
+run()
